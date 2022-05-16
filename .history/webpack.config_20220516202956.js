16651df82ddffe7463919cff92844faff80cf332
yangyanni999 <1587240774@qq.com>
2022-05-16 20:42:27 +0800

    path bug 未修复

---
+++ .history/webpack.config_20220516202956.js
@@ -0,0 +1,16 @@
+const config = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path:'./bin'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {test:'/\.css$/',use:'css-loader'}
+    ]
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({template:'./preview.html'})//指定预浏览页面
+  // ]
+}
